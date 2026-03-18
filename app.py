cd040c5d59fa249bc76061724bf2b2555b15c9e6
mr-anonymous404 <someoneanonymous4020@gmail.com>
2026-03-18 23:28:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.json.sort_keys = False
 
 apiKeys = {
-    "personal": "anonx561",
-    "temp": "temp043"
+    "personal": "someone#5006v",
+    "temp": "tempx183"
 }
 
 def format_response(success: bool, data: dict, rc_number: str):
